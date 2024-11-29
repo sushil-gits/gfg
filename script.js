a5a9e7d60b56cc293d37befa88651cc540dd4fdc
sushil kumar <sushilraj201020@gmail.com>
2024-11-29 20:58:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// non primtive datatypes
+
+// // array
+// let arr=[10,"sushil",30];
+// console.log(arr)
+
+// // object key:value
+// let obj={
+//     user_name:"sushil",
+//     bank:3246951380743,
+//     balance:4567
+// }
+// console.log(obj)
+// // function
+// let fun=function(){
+//     console.log("ka be kaisen hi")
+//     return "kal";
+// }
+// // fun();
+// // console.log(typeof fun)
+// console.log(fun())
+
+// type conversion
+// let amount="300";
+// let num=Number(amount);
+// console.log(num);
+// console.log(typeof num);
+// console.log(typeof amount);
+// boolean to number
+// let x=true;
+// console.log(Number(x));
+// let total="3897th"; //not a number
+// let num=Number(total);
+// console.log(num);
+
+// null
+// let x1=null;
+// // console.log(Number(x1))
+
+// // boolean
+// let x2="ghar jaao"
+// console.log(Boolean(x2))
+
+// // arithmetic operator
+// console.log((5+(7*6))/2)
+// let total=67;
+// total--
+// console.log(total)
+
+// assignment operator
+// let s=45;
+// s=78
+// console.log(s)
+// // comparison operator
+// let p=56;
+// let r="klp";
+// console.log(p==r)
+// logical operator
+let a=567;
+let v=null;
+console.log(a&&v)
+console.log(a||v)
+
+
